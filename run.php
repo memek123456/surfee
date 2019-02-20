03a328716fe87c7bdb03f48945fae115a0715c0f
memek123456 <46232817+memek123456@users.noreply.github.com>
2019-02-20 13:59:22 +0700

    Add files via upload

---
+++ run.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'init_surfee.php';
+//config
+//isi code auth token
+$auth='xxxxxxxxxxx';
+//isi xxxx dgn benar
+$gps='latitude=xxxxxxx&longitude=xxxxxxxxx';
+
+
+//run bot
+survey($auth,$gps);

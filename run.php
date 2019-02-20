382b8f96a94a2f5d74fa4620e7bab0a0eaf9f66b
memek123456 <46232817+memek123456@users.noreply.github.com>
2019-02-20 14:08:52 +0700

    Update run.php

--- run.php
+++ run.php
@@ -2,9 +2,9 @@
 require_once 'init_surfee.php';
 //config
 //isi code auth token
-$auth='xxxxxxxxxxx';
+$auth='eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MTIyMzI4LCJleHAiOjE3MDg0MTE2NzQsImlhdCI6MTU1MDY0NTI3NH0.m56kB8QJP83u4wrGGRB2eeQvTfPEPOQeDLlypnpivno';
 //isi xxxx dgn benar
-$gps='latitude=xxxxxxx&longitude=xxxxxxxxx';
+$gps='latitude=-6.8342625&longitude=106.7596106';
 
 
 //run bot
